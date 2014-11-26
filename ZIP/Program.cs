222810c02bde6f1296dd380110cf2a23678c0246
Danylo Fitel <danylo.fitel@outlook.com>
2014-11-26 20:19:05 +0200

    Updated demo with checking for correctness.

---
+++ ZIP/Program.cs
@@ -0,0 +1,92 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="Program.cs" company="FTL">
+//     FTL Inc.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace Zip
+{
+    using System;
+    using System.Linq;
+
+    /// <summary>
+    /// Demo class
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// Main method.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
+        public static void Main(string[] args)
+        {
+            /*
+            Test("абвгґдеєжзиіїйклмнопрстуфхцчшщьюя_!", "привіт_віка_сподіваюсь_ти_вже_здорова_в_мене_все_чудово_скучаю_чекаю!");
+            Console.WriteLine();
+            Test("benort", string.Concat(Enumerable.Repeat("tobeornottobeor", 1000)) + "tobe");
+             * */
+
+            do
+            {
+                Console.WriteLine("Alphabet:");
+                var alphabet = Console.ReadLine();
+                try
+                {
+                    var zipper = new ZipEncrypt(alphabet);
+                    var unzipper = new ZipDecrypt(alphabet);
+                    do
+                    {
+                        Console.WriteLine("Message:");
+                        var message = Console.ReadLine();
+                        try
+                        {
+                            var encrypted = zipper.Encrypt(message);
+                            Console.WriteLine(encrypted);
+                            if (unzipper.Decrypt(encrypted) != message)
+                            {
+                                Console.WriteLine("Oops, I fucked up a bit.");
+                            }
+                        }
+                        catch (Exception)
+                        {
+                            Console.WriteLine("You idiot, this message is fucking wrong!");
+                        }
+
+                        Console.WriteLine("Another message?");
+                    }
+                    while (Console.ReadLine().ToLower() != "n");
+                    Console.WriteLine("Another alphabet?");
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("You idiot, this is not a fucking alphabet, go back to school!");
+                }
+            }
+            while (Console.ReadLine().ToLower() != "n");
+        }
+
+        /// <summary>
+        /// Tests the specified alphabet and message.
+        /// </summary>
+        /// <param name="alphabet">The alphabet.</param>
+        /// <param name="message">The message.</param>
+        private static void Test(string alphabet, string message)
+        {
+            var zipper = new ZipEncrypt(alphabet);
+            var unzipper = new ZipDecrypt(alphabet);
+            var sw = new System.Diagnostics.Stopwatch();
+
+            sw.Start();
+            var encrypted = zipper.Encrypt(message);
+            Console.WriteLine("Encrypted in {0} milliseconds", sw.ElapsedMilliseconds);
+            Console.WriteLine(encrypted);
+
+            sw.Restart();
+            var decrypted = unzipper.Decrypt(encrypted);
+            Console.WriteLine("Decrypted in {0} milliseconds", sw.ElapsedMilliseconds);
+
+            Console.WriteLine("Original size: {0}; encrypted size: {1}", message.Length, encrypted.Length);
+            Console.WriteLine(message == decrypted ? "Success!!!" : "Fail...");
+        }
+    }
+}
